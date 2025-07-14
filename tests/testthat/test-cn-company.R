b6df26892986e64ab3c671a03dd0ce1153cd641f
Tan Changde <tanchangde@gmail.com>
2025-07-14 15:15:41 +0800

    fix: typos

--- tests/testthat/test-cn-company.R
+++ tests/testthat/test-cn-company.R
@@ -35,8 +35,8 @@ test_that("cn_com_shareholders_num response is correctly converted to a data fra
   expect_true(all(purrr::map_lgl(result, ~ !is.list(.))))
 })
 
-test_that("cn_com_major_shareholders_shares_change response is correctly converted to a data frame", {
-  result <- mock_lxr_unnest(test_path("fixtures", "cn_com_trading_abnormal_resp.json"))
+test_that("cn_com_senior_executive_shares_change response is correctly converted to a data frame", {
+  result <- mock_lxr_unnest(test_path("fixtures", "cn_com_senior_executive_shares_change.json"))
 
   expect_true(is.data.frame(result))
   expect_true(all(purrr::map_lgl(result, ~ !is.list(.))))
