9e00b9f9dbe34ba7da407ca5e7e30e77f36650ef
Tan Changde <tanchangde@gmail.com>
2025-07-14 14:51:46 +0800

    style: simplify object names

--- data-raw/cn-company-resp.R
+++ data-raw/cn-company-resp.R
@@ -11,29 +11,29 @@ save_json_response <- function(resp, path) {
     write(path)
 }
 
-resp_lxr_cn_com <- lxr_cn_com(
+resp_cn_com <- lxr_cn_com(
   token = Sys.getenv("LIXINGR_TOKEN"),
   stock_code = c("300750", "600519", "600157"),
   .return_format = "resp"
 )
 
 save_json_response(
-  resp_lxr_cn_com,
+  resp_cn_com,
   "./tests/testthat/fixtures/cn_com_resp.json"
 )
 
-resp_lxr_cn_com_profile <- lxr_cn_com_profile(
+resp_cn_com_profile <- lxr_cn_com_profile(
   token = Sys.getenv("LIXINGR_TOKEN"),
   stock_codes = c("300750", "600519", "600579"),
   .return_format = "resp"
 )
 
 save_json_response(
-  resp_lxr_cn_com_profile,
+  resp_cn_com_profile,
   "./tests/testthat/fixtures/cn_com_profile.json"
 )
 
-resp_lxr_cn_com_equity_change <- lxr_cn_com_equity_change(
+resp_cn_com_equity_change <- lxr_cn_com_equity_change(
   token = Sys.getenv("LIXINGR_TOKEN"),
   stock_code = "300750",
   start_date = "2024-10-01",
@@ -43,11 +43,11 @@ resp_lxr_cn_com_equity_change <- lxr_cn_com_equity_change(
 )
 
 save_json_response(
-  resp_lxr_cn_com_equity_change,
+  resp_cn_com_equity_change,
   "./tests/testthat/fixtures/cn_com_equity_change.json"
 )
 
-resp_lxr_cn_com_candlestick <- lxr_cn_com_candlestick(
+resp_cn_com_candlestick <- lxr_cn_com_candlestick(
   token = Sys.getenv("LIXINGR_TOKEN"),
   stock_code = "300750",
   type = "ex_rights",
@@ -58,11 +58,11 @@ resp_lxr_cn_com_candlestick <- lxr_cn_com_candlestick(
 )
 
 save_json_response(
-  resp_lxr_cn_com_candlestick,
+  resp_cn_com_candlestick,
   "./tests/testthat/fixtures/cn_com_candlestick.json"
 )
 
-resp_lxr_cn_com_shareholders_num <- lxr_cn_com_shareholders_num(
+resp_cn_com_shareholders_num <- lxr_cn_com_shareholders_num(
   token = Sys.getenv("LIXINGR_TOKEN"),
   stock_code = "300750",
   start_date = "2024-06-01",
@@ -71,11 +71,11 @@ resp_lxr_cn_com_shareholders_num <- lxr_cn_com_shareholders_num(
 )
 
 save_json_response(
-  resp_lxr_cn_com_shareholders_num,
+  resp_cn_com_shareholders_num,
   "./tests/testthat/fixtures/cn_com_shareholders_num.json"
 )
 
-resp_lxr_cn_com_trading_abnormal <- lxr_cn_com_trading_abnormal(
+resp_cn_com_trading_abnormal <- lxr_cn_com_trading_abnormal(
   token = Sys.getenv("LIXINGR_TOKEN"),
   start_date = "2020-03-01",
   end_date = "2025-07-01",
@@ -84,6 +84,6 @@ resp_lxr_cn_com_trading_abnormal <- lxr_cn_com_trading_abnormal(
 )
 
 save_json_response(
-  resp_lxr_cn_com_trading_abnormal,
+  resp_cn_com_trading_abnormal,
   "./tests/testthat/fixtures/cn_com_trading_abnormal_resp.json"
 )
