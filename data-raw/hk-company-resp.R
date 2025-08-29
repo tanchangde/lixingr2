050d84acc7832d91d16af230787d8d5baab0874a
Tan Changde <tanchangde@gmail.com>
2025-08-29 19:31:58 +0800

    feat: add lxr_hk_com_fs_non_financial endpoint

--- data-raw/hk-company-resp.R
+++ data-raw/hk-company-resp.R
@@ -62,3 +62,18 @@ save_json_response(
   resp_hk_com_fundmtl_non_financial,
   "./tests/testthat/fixtures/hk_com_fundmtl_non_financial.json"
 )
+
+resp_hk_com_fs_non_financial <- lxr_hk_com_fs_non_financial(
+  token = Sys.getenv("LIXINGR_TOKEN"),
+  start_date = "2024-07-01",
+  end_date = "2025-07-01",
+  stock_codes = "00700", ,
+  metrics_list = "q.ps.toi.t",
+  limit = 5,
+  .config = list(return_format = "resp")
+)
+
+save_json_response(
+  resp_hk_com_fs_non_financial,
+  "./tests/testthat/fixtures/hk_com_fs_non_financial.json"
+)
